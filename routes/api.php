ca5960d5693632047361f7c049a216cb2d63751f
Asep Oo Kurniawan <kurniawan9411n@gmail.com>
2023-02-15 23:18:13 +0700

    add auth API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\FileManager;
 use App\Http\Controllers\Api\Presentation;
 use Illuminate\Http\Request;
@@ -20,7 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('logedinOnly')->prefix("/file-manager")->controller(FileManager::class)
+Route::middleware('auth:sanctum')->prefix("/file-manager")->controller(FileManager::class)
     ->group(function () {
         Route::get("/dirs", "handleDirListing");
         Route::get("/list", "handleListContent");
@@ -32,4 +33,9 @@ Route::prefix("/presentation")->controller(Presentation::class)
     ->group(function () {
         Route::get("/view/{fileuuid}", "handleViewFileByUuid")->name("api.view_url");
         Route::get("/download/{fileuuid}", "handleDownloadFileByUuid");
+    });
+
+Route::prefix("/auth")->controller(AuthController::class)
+    ->group(function () {
+        Route::post("/login", "login");
     });
